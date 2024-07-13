17f46db02cd245edbcf1309d4d33335dc808234e
ᴛʜᴀᴋᴜʀ ᴀʙʜᴀʏ ꜱɪɴɢʜ <ks8817112@gmail.com>
2024-07-13 15:16:10 +0530

    callback.py

--- DAXXMUSIC/plugins/admins/callback.py
+++ DAXXMUSIC/plugins/admins/callback.py
@@ -179,7 +179,7 @@ async def del_back_playlist(client, CallbackQuery, _):
     elif command == "Skip" or command == "Replay":
         check = db.get(chat_id)
         if command == "Skip":
-            txt = f"➻ sᴛʀᴇᴀᴍ sᴋɪᴩᴩᴇᴅ 🎄\n│ \n└ʙʏ : {mention} 🥀\n│ \n└➻ ᴍᴀᴅᴇ ʙʏ ☠️ ᴛʜᴀᴋᴜʀ ᴀʙʜᴀʏ ☠️"
+            txt = f"➻ sᴛʀᴇᴀᴍ sᴋɪᴩᴩᴇᴅ 🎄\n│ \n└ʙʏ : {mention} 🥀\n│ \n└➻ ᴍᴀᴅᴇ ʙʏ ⟜⃜᷼🐰⃝⃪⃕͜.🇷OMEO⎯⳰͢❥๋"
             popped = None
             try:
                 popped = check.pop(0)
@@ -187,7 +187,7 @@ async def del_back_playlist(client, CallbackQuery, _):
                     await auto_clean(popped)
                 if not check:
                     await CallbackQuery.edit_message_text(
-                        f"➻ sᴛʀᴇᴀᴍ sᴋɪᴩᴩᴇᴅ 🎄\n│ \n└ʙʏ : {mention} 🥀\n│ \n└➻ ᴍᴀᴅᴇ ʙʏ ☠️ ᴛʜᴀᴋᴜʀ ᴀʙʜᴀʏ ☠️"
+                        f"➻ sᴛʀᴇᴀᴍ sᴋɪᴩᴩᴇᴅ 🎄\n│ \n└ʙʏ : {mention} 🥀\n│ \n└➻ ᴍᴀᴅᴇ ʙʏ ⟜⃜᷼🐰⃝⃪⃕͜.🇷OMEO⎯⳰͢❥๋"
                     )
                     await CallbackQuery.message.reply_text(
                         text=_["admin_6"].format(
@@ -202,7 +202,7 @@ async def del_back_playlist(client, CallbackQuery, _):
             except:
                 try:
                     await CallbackQuery.edit_message_text(
-                        f"➻ sᴛʀᴇᴀᴍ sᴋɪᴩᴩᴇᴅ 🎄\n│ \n└ʙʏ : {mention} 🥀\n│ \n└➻ ᴍᴀᴅᴇ ʙʏ ☠️ ᴛʜᴀᴋᴜʀ ᴀʙʜᴀʏ ☠️"
+                        f"➻ sᴛʀᴇᴀᴍ sᴋɪᴩᴩᴇᴅ 🎄\n│ \n└ʙʏ : {mention} 🥀\n│ \n└➻ ᴍᴀᴅᴇ ʙʏ ⟜⃜᷼🐰⃝⃪⃕͜.🇷OMEO⎯⳰͢❥๋"
                     )
                     await CallbackQuery.message.reply_text(
                         text=_["admin_6"].format(
@@ -214,7 +214,7 @@ async def del_back_playlist(client, CallbackQuery, _):
                 except:
                     return
         else:
-            txt = f"➻ sᴛʀᴇᴀᴍ ʀᴇ-ᴘʟᴀʏᴇᴅ 🎄\n│ \n└ʙʏ : {mention} 🥀\n│ \n└➻ ᴍᴀᴅᴇ ʙʏ ☠️ ᴛʜᴀᴋᴜʀ ᴀʙʜᴀʏ ☠️"
+            txt = f"➻ sᴛʀᴇᴀᴍ ʀᴇ-ᴘʟᴀʏᴇᴅ 🎄\n│ \n└ʙʏ : {mention} 🥀\n│ \n└➻ ᴍᴀᴅᴇ ʙʏ ⟜⃜᷼🐰⃝⃪⃕͜.🇷OMEO⎯⳰͢❥๋"
         await CallbackQuery.answer()
         queued = check[0]["file"]
         title = (check[0]["title"]).title()
