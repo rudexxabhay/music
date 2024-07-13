7ae0bf2358c6504c06d574d182a16dd65e589916
ᴛʜᴀᴋᴜʀ ᴀʙʜᴀʏ ꜱɪɴɢʜ <ks8817112@gmail.com>
2024-07-13 15:27:39 +0530

    config.py

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ DEEP_API = getenv("DEEP_API")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/RudeXAbhay/success/",
+    "https://github.com/rudexxabhay/music/",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
